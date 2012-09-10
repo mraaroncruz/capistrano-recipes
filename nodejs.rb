cd3cf12f9f1e56259deff68c915ecfee767791f1
Aaron Cruz <aaron@aaroncruz.com>
2012-09-10 12:58:13 +0200

    Just added -y instead of prompt stuff

--- nodejs.rb
+++ nodejs.rb
@@ -1,16 +1,7 @@
 namespace :nodejs do
   desc "Install the latest relase of Node.js"
   task :install, roles: :app do
-    run "#{sudo} add-apt-repository ppa:nginx/stable",:pty => true do |ch, stream, data|
-      if data =~ /Press.\[ENTER\].to.continue/
-        #prompt, and then send the response to the remote process
-        ch.send_data(Capistrano::CLI.password_prompt("Press enter to continue:") + "\n")
-      else
-        #use the default handler for all other text
-        Capistrano::Configuration.default_io_proc.call(ch,stream,data)
-      end
-    end
-
+    run "#{sudo} add-apt-repository -y ppa:chris-lea/node.js"
     run "#{sudo} apt-get -y update"
     run "#{sudo} apt-get -y install nodejs"
   end
