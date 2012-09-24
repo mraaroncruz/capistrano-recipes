353cf38c408e1815a6c709868c71a6e6ab44e9c1
Aaron Cruz <aaron@aaroncruz.com>
2012-09-24 14:54:06 +0200

    Updated to rails init path

--- el_jefe.rb
+++ el_jefe.rb
@@ -1,6 +1,6 @@
 namespace :el_jefe do
   task :symlink, roles: :app do
-    run "ln -nfs #{shared_path}/config/el_jefe.yml #{release_path}/config/el_jefe.yml"
+    run "ln -nfs #{shared_path}/config/el_jefe.yml #{release_path}/config/initializers/el_jefe.yml"
   end
   after "deploy:finalize_update", "el_jefe:symlink"
 end
