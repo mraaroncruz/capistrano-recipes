52f0afa83feb2c2b39d7c8c8dfaa2af032f5a0b1
Aaron Cruz <aaron@aaroncruz.com>
2012-10-01 18:36:56 +0200

    Added htop

--- logs.rb
+++ logs.rb
@@ -3,4 +3,10 @@ namespace :logs do
   task :tail do
     stream "tail -n 300 -f #{current_path}/log/#{stage}.log"
   end
+
+  desc "View htop"
+  task :htop do
+    hostname = find_servers_for_task(current_task).first
+    exec "ssh -t #{hostname} 'htop'"
+  end
 end
