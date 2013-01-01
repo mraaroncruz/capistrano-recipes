61d51001c015040a0144d5821839a6e642a7033b
Aaron Cruz <aaron@aaroncruz.com>
2013-01-01 20:15:38 +0100

    Updated a couple messups

--- backup.rb
+++ backup.rb
@@ -1,14 +1,16 @@
+set_default(:backup_postgres_socket_path, "/var/run/postgres")
 set_default(:backup_encryption_password) { Capistrano::CLI.password_prompt "Choose an encryption password: " }
 set_default(:backup_backup_server_host)  { Capistrano::CLI.password_prompt "What is your backup server's hostname? " }
 set_default(:backup_backup_server_user)  { Capistrano::CLI.password_prompt "What is your backup server's username? " }
 set_default(:backup_backup_server_path)  { Capistrano::CLI.password_prompt "What is the backup path on your backup server? " }
+set_default(:backup_rsync_push_directories) { ["#{shared_path}/system","#{shared_path}/config"] }
 
 namespace :backup do
   desc "Install the backup gem."
   task :install do
-    run "#{sudo} gem install backup net-ssh"
-    template "backup/backup.yml", "#{shared_path}/config/backup.yml"
-    template "backup/recipe.rb", "#{shared_path}/config/recipe.rb"
+    run "#{sudo} gem install backup net-ssh --no-rdoc --no-ri"
+    template "backup/backup.yml.erb", "#{shared_path}/config/backup.yml"
+    template "backup/recipe.rb.erb", "#{shared_path}/config/recipe.rb"
   end
   after "deploy:install", "backup:install"
 
